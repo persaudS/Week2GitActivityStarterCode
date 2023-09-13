de0a25433a67d3b2ab2e9cbf25182e3223695d62
Maeve Wade <maevewade@Maeves-MacBook-Pro.local>
2023-09-13 12:21:10 -0400

    implement divide operation

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import { Calculator } from './calculator';
+import { Calculator } from "./calculator";
 
 const calc = new Calculator();
 
 const result_add = calc.add(2, 3);
 const result_sub = calc.sub(3, 2);
+const result_div = calc.div(10, 2);
 console.log(`result_add = ${result_add}`);
 console.log(`result_sub = ${result_sub}`);
 const result_mult = calc.mult(3, 4);
-console.log(`result_mult = ${result_mult}`); 
+console.log(`result_mult = ${result_mult}`);
+console.log(`result_div = ${result_div}`);
